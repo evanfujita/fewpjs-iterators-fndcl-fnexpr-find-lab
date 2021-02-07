dcefb1d0118e059f8fa82e034db605c861539054
Evan <evanfujita@gmail.com>
2021-02-06 20:19:24 -0600

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+let superbowlWin = record => {
+
+  let result = record.find(s => s['result'] === 'W')
+  return !!result ?  result.year : undefined
+
+}
