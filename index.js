a4418b8ab93d256d30973ca2d5af411370ee688c
Evan <evanfujita@gmail.com>
2021-02-06 20:19:18 -0600

    commit tree 799f82e5e53d286196d1814ab7a317f80518c005

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+let superbowlWin = record => {
+
+  let result = record.find(s => s['result'] === 'W')
+  return !!result ?  result.year : undefined
+  
+
+}
